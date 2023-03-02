f83271f5e6f55307d57ad8eab1a76334232de8cd
Dmitrii Shmidt <shmicer@gmail.com>
2023-03-02 21:14:22 +0300

    Change views lo class-based2

--- app/todo/models.py
+++ app/todo/models.py
@@ -8,15 +8,15 @@ class Category(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
 
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse('category', kwargs={'cat_slug': self.slug})
-
-    class Meta:
-        verbose_name = "Category"
-        verbose_name_plural = "Categories"
 
 
 class ToDoList(models.Model):
